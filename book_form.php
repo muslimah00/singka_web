3dbcf6e0d27aa1f94f0d08e5bce476d86e574eb8
muslimah00 <muslimah.mbs@gmail.com>
2023-01-13 23:31:50 +0700

    Update book_form.php

--- book_form.php
+++ book_form.php
@@ -12,10 +12,11 @@
       $arrivals = $_POST['arrivals'];
       $leaving = $_POST['leaving'];
 
-      $request = " insert into book_form(name, email, phone, address, location, guests, arrivals, leaving) values('$name','$email','$phone','$address','$location','$guests','$arrivals','$leaving') ";
-      mysqli_query($connection, $request);
+      //$request = " insert into book_form(name, email, phone, address, location, guests, arrivals, leaving) values('$name','$email','$phone','$address','$location','$guests','$arrivals','$leaving') ";
+      //mysqli_query($connection, $request);
 
-      header('location:book.php'); 
+      //header('location:book.php');
+      header("location:https://api.whatsapp.com/send?phone=087777214369&text=Name:%20$name%20%0DEmail:%20$email%20%0DAddress:%20$address%20%0DLocation:%20$location%20%0DGuests:%20$guests%20%0DArrivels:%20$arrivals%20%0DLeaving:%20$leaving");
 
    }else{
       echo 'something went wrong please try again!';
